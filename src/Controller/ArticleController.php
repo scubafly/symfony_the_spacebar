80eedfb0f351d6c2783f7082bd313e5a7d3c0b95
Jack Over <jack@limoengroen.nl>
2020-01-16 14:17:08 +0100

    First route created with dynamic slug

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,9 +1,28 @@
 <?php
 
-
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController {
+
+  /**
+   * @Route("/")
+   */
+  public function homepage() {
+    return new Response('WHOEOE! This is the homepage.');
+  }
+
+  /**
+   * @Route("/news/{slug}")
+   */
+  public function show($slug) {
+    return new Response(
+      sprintf('Future page to show the article: %s',
+      $slug
+      )
+    );
+  }
 
 }
